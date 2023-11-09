243cf3a25a4e0706c94dccf428341f1cfc578dcf
thadeus <thadeusnyariki94@gmail.com>
2023-11-09 18:31:31 +0300

    variables

---
+++ Main.java
@@ -0,0 +1,23 @@
+/**
+ * Variables are containers for storing data values
+ * In java, there are different types of variables, e.g String, int, float, char, booloean
+ * To declare a variable, you must specify the type and assign it a value; e.g type variableName = value;
+ */
+
+public class Main {
+    public static void main(String[] args){
+        String name = "John";
+        int myNum = 15;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(name);
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}

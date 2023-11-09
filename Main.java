4f25ec82f141aaeba5b5333d5640b9a5e69a68d9
thadeus <thadeusnyariki94@gmail.com>
2023-11-09 19:27:34 +0300

    declare multiple variables

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * To declare more than one variable of the same type, you can use a comma-separated list
+ */
+
+/**
+ * You can also assign the same value to multiple variables in one line:
+ */
+
+public class Main {
+    public static void main(String[] args){
+        int x = 5, y = 6, z = 50;
+
+        int a, b, c;
+        a = b = c = 50;
+
+        System.out.println(x + y + z);       
+        System.out.println(a + b + c);
+    }
+}

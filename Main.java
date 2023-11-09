e68c79f44e81a49884fb782345ef15d9b8552aa5
thadeus <thadeusnyariki94@gmail.com>
2023-11-09 17:55:40 +0300

    get started

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * In java, every application begins with a class name, and that class must match the filename.
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}

76a0cc9fb1006a9a683150535676ac35322f0343
thadeus <thadeusnyariki94@gmail.com>
2023-11-10 21:28:56 +0300

    Data types

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * Data types are divided into two groups:
+ * Primitive data types - byte, short, int, long, float, double, boolean and char.
+ * Non-primitive data types - String, Arrays and Classes
+ */
+
+public class Main{
+    public static void main(String[] args){
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}

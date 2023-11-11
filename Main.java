6f2c2c5aa0788029ba8ac073be521ba79618cbe6
thadeus <thadeusnyariki94@gmail.com>
2023-11-11 12:41:31 +0300

    Short-Hand-If-Else

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * variable = (condition) ? expressionTrue :  expressionFalse;
+ */
+
+public class Main {
+    public static void main(String[] args){
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+    }
+}

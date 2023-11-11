ae49c44d60dd81054296d0fc9bd69fb84e1a4e94
thadeus <thadeusnyariki94@gmail.com>
2023-11-11 15:37:41 +0300

    While-Loop

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * The while loop loops through a block of code as long as a specified condition is true:
+ */
+
+public class Main {
+    public static void main(String[] args){
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

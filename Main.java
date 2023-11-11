46ccc89f148a530f0fe7755fd6eae30ddcdf9d9f
thadeus <thadeusnyariki94@gmail.com>
2023-11-11 11:28:49 +0300

    Math

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * The Math.max(x,y) method can be used to find the highest value of x and y:
+ * 
+ * The Math.min(x,y) method can be used to find the lowest value of x and y:
+ * 
+ * The Math.sqrt(x) method returns the square root of x:
+ * 
+ * The Math.abs(x) method returns the absolute (positive) value of x:
+ * 
+ * Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(Math.max(5, 10));
+        System.out.println(Math.min(5, 10));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.random());
+    }
+}

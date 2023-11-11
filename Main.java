0ce1633e70b5c5ce7d6fa245a8b4175adad96c07
thadeus <thadeusnyariki94@gmail.com>
2023-11-11 16:14:55 +0300

    For-Each-Loop

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * a "for-each" loop, is used exclusively to loop through elements in an array:
+ */
+
+public class Main {
+    public static void main (String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}

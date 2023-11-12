acc94b34f1ffad2c10b35328c4185676ecc2ab41
thadeus <thadeusnyariki94@gmail.com>
2023-11-12 21:26:13 +0300

    Modify-Attributes

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    int x, y = 10;
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        myObj.x = 40;
+        myObj.y = 25;
+        System.out.println(myObj.x);
+        System.out.println(myObj.y);
+    }
+}

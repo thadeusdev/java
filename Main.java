ded44f8c8eb482bcde385ef16a9fb872070ac7f5
thadeus <thadeusnyariki94@gmail.com>
2023-11-12 21:16:57 +0300

    Class-Attributes

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Create a class called "Main" with two attributes: x and y:
+ */
+
+public class Main {
+    int x = 5;
+    int y = 3;
+
+    // Create an object called "myObj" and print the value of x and y:
+    public static void main(String[] args){
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+        System.out.println(myObj.y);
+    }
+}

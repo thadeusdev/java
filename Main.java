f9661a2bfae79dca6fb385b9dbd968fdc2593f38
thadeus <thadeusnyariki94@gmail.com>
2023-11-12 14:45:35 +0300

    Halting-Condition

---
+++ Main.java
@@ -0,0 +1,23 @@
+/**
+ * Just as loops can run into the problem of infinite looping, recursive functions can run into the problem of infinite recursion. Infinite recursion is when the function never stops calling itself. Every recursive function should have a halting condition, which is the condition where the function stops calling itself. In the previous example, the halting condition is when the parameter k becomes 0.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int result = sum(5, 10);
+        System.out.println(result);
+    }
+
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
+}
+
+// NOTE
+/*
+ * The developer should be very careful with recursion as it can be quite easy to slip into writing a function which never terminates, or one that uses excess amounts of memory or processor power. However, when written correctly recursion can be a very efficient and mathematically-elegant approach to programming.
+ */

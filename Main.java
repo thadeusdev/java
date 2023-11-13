2375d4db51e4a09f09043001b1504f0c17f037d4
thadeus <thadeusnyariki94@gmail.com>
2023-11-13 16:30:34 +0300

    Modifiers

---
+++ Main.java
@@ -0,0 +1,20 @@
+// Access Modifiers
+/**
+ * public - The code is accessible for all classes
+ * private - The code is only accessible within the declared class
+ * default - The code is only accessible in the same package. This is used when you don't specify a modifier.
+ * protected - The code is accessible in the same package and subclasses
+ */
+
+// Non-access Modifiers
+/**
+ * final - Attributes and methods cannot be overriden/modified
+ * static - Attributes and methods belongs to the class, rather than an object
+ * abstract - Can only be used in an abstract class, and can only be used on methods. The method does not have a body,for example abstract void run();. The body is provided by the subclass (inherited from).
+ * transient - attributes and methods are skipped when serializing the object containing them
+ * synchronized - Methods vcan ony be accessed by one thread at a time
+ * volatile - The value of an attribute is not cached thread-locally, and is always read from the "main memory"
+ */
+public class Main {
+    
+}

54cabbc53dfd0745d65c072d3518fa3ad9f1ad67
thadeus <thadeusnyariki94@gmail.com>
2023-11-16 21:49:02 +0300

    Polymorphism

---
+++ Animal.java
@@ -0,0 +1,13 @@
+/**
+ * Polymorphism - means "many forms", and it occurs when we have many classes that are related to each other by inheritance.
+ * 
+ * Inheritance - lets us inherit attributes and methods from another class.
+ * 
+ * Polymorphism uses those methods to perform different tasks. - this allows us to perform a single action in different ways.
+ */
+
+class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+}

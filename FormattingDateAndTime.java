4ce78bebe2f8a9d093edcc20f9ce8c99a800b298
thadeus <thadeusnyariki94@gmail.com>
2023-11-20 12:22:12 +0300

    Date-and-Time

---
+++ FormattingDateAndTime.java
@@ -0,0 +1,17 @@
+/**
+ *  use the "DateTimeFormatter" class with the "ofPattern()" method in the same package to format or parse date-time objects
+ */
+
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+public class FormattingDateAndTime {
+    public static void main(String[] args) {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);
+    }
+}
